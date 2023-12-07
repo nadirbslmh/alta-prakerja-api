5ce03ca94f0a1cf2236d8dd59c03d7632ca3d72d
nadirbasalamah <nadir008basalamah@gmail.com>
2023-12-07 17:07:43 +0700

    feat(shutdown): add graceful shutdown

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"context"
 	"gugcp/database"
 	"gugcp/routes"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
+
+type operation func(ctx context.Context) error
 
 func main() {
 	database.InitDB()
@@ -14,5 +24,67 @@ func main() {
 
 	routes.InitRoutes(e)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	go func() {
+		if err := e.Start(":8080"); err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	wait := gracefulShutdown(context.Background(), 2*time.Second, map[string]operation{
+		"database": func(ctx context.Context) error {
+			return database.DB.Close()
+		},
+		"http-server": func(ctx context.Context) error {
+			return e.Shutdown(context.Background())
+		},
+	})
+
+	<-wait
+}
+
+func gracefulShutdown(ctx context.Context, timeout time.Duration, ops map[string]operation) <-chan struct{} {
+	wait := make(chan struct{})
+	go func() {
+		s := make(chan os.Signal, 1)
+
+		// add any other syscalls that you want to be notified with
+		signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+		<-s
+
+		log.Println("shutting down")
+
+		// set timeout for the ops to be done to prevent system hang
+		timeoutFunc := time.AfterFunc(timeout, func() {
+			log.Printf("timeout %d ms has been elapsed, force exit", timeout.Milliseconds())
+			os.Exit(0)
+		})
+
+		defer timeoutFunc.Stop()
+
+		var wg sync.WaitGroup
+
+		// Do the operations asynchronously to save time
+		for key, op := range ops {
+			wg.Add(1)
+			innerOp := op
+			innerKey := key
+			go func() {
+				defer wg.Done()
+
+				log.Printf("cleaning up: %s", innerKey)
+				if err := innerOp(ctx); err != nil {
+					log.Printf("%s: clean up failed: %s", innerKey, err.Error())
+					return
+				}
+
+				log.Printf("%s was shutdown gracefully", innerKey)
+			}()
+		}
+
+		wg.Wait()
+
+		close(wait)
+	}()
+
+	return wait
 }
